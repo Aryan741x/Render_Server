66be15ea39cb352faa8e3697c11517ff5d1ec58e
Aryan Sharma <128162387+Aryan741x@users.noreply.github.com>
2025-07-21 15:35:02 +0530

    Test-1

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from flask import Flask
-from flask_cors import CORS
-from routes import routes
-
 app = Flask(__name__)
-CORS(app, origins="*")
-app.register_blueprint(routes, url_prefix="")
-
-# Done: Do NOT call app.run()
+@app.route("/")
+def hello():
+    return "Hello from Render!"
+# Do NOT add app.run()
